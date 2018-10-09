c72c399aade7fa0573705fe2802c6362caef5797
Haim Rubin <haim.rubin@gmail.com>
2018-10-09 14:38:28 +0300

    Extract client/server into frontend/backend folders

--- backend/index.js
+++ backend/index.js
@@ -2,10 +2,11 @@ const express = require('express')
 const app = express()
 const path = require('path')
 const port = 3000
-app.use('/resources',express.static('resources'))
+const CLIENT_RESOURCES = 'frontend'
+app.use('/resources',express.static(`${CLIENT_RESOURCES}/resources`))
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname ,'../index.html'))
+    res.sendFile(path.join(__dirname ,`../${CLIENT_RESOURCES}/index.html`))
 })
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
---
+++ frontend/script.js
@@ -0,0 +1,6 @@
+function init(dep){
+
+}
+console.log('ddddd')
+alert('sfdf')
+
